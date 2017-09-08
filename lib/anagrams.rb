d2e1ba907628f7ecb285037642ca825674025f0f
Mitchell Long <meechllada@gmail.com>
2017-09-08 10:40:17 -0700

    account for case sensitivity and pass second test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,5 @@
+require('pry')
+
 class EvaluateWord
   attr_reader(:first_word, :second_word)
   def initialize (first_word,second_word)
@@ -7,18 +9,23 @@ class EvaluateWord
 
 
   def anagram?
+
+
     first_word_key = @first_word.downcase.split('').sort.join('')
     second_word_key = @second_word.downcase.split('').sort.join('')
-    if first_word_key == second_word_key
-      true
+    if first_word_key == second_word_key && @first_word.length == @second_word.length
+       true
+    else
+       false
     end
+
   end
 end
 
 
-# puts "Please enter your first word"
-# first_entry = gets.chomp
-# puts "Please enter your second word"
-# second_entry = gets.chomp
-# word_entries = EvaluateWord.new(first_entry,second_entry)
-# #the above variable isnt linked to the entries, must do that
+puts "Please enter your first word"
+first_entry = gets.chomp
+puts "Please enter your second word"
+second_entry = gets.chomp
+word_entries = EvaluateWord.new(first_entry,second_entry)
+puts word_entries.anagram?()
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,7 +4,11 @@ require('pry')
 
 describe('#Anagram_checker')  do
  it("returns true if the two words inputted are anagrams") do
- words_to_compare = EvaluateWord.new("tea", "eat")
- expect(words_to_compare.anagram?()).to(eq(true))
+   compare_words = EvaluateWord.new("tea", "eat")
+   expect(compare_words.anagram?()).to(eq(true))
+ end
+ it("accounts for entries of different cases") do
+   compare_words = EvaluateWord.new("Tea", "eAt")
+   expect(compare_words.anagram?()).to(eq(true))
  end
 end
