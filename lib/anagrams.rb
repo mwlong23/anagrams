b0da75f7d809248db2b7bb2c0632dbf1221661cb
Mitchell Long <meechllada@gmail.com>
2017-09-08 14:05:38 -0700

    refactor long variables

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,31 +1,59 @@
 require('pry')
 
 class EvaluateWord
-  attr_reader(:first_word, :second_word)
-  def initialize (first_word,second_word)
-    @first_word = first_word
-    @second_word = second_word
+  attr_reader(:first_phrase, :second_phrase)
+  def initialize (first_phrase,second_phrase)
+    @first_phrase = first_phrase.downcase
+    @second_phrase = second_phrase.downcase
+    @first_phrase_array = @first_phrase.split(' ')
+    @second_phrase_array = @second_phrase.split(' ')
   end
 
 
+
+  # def word?
+  #   puts  first_phrase_array = first_word.downcase.split(' ')
+  #   puts  second_phrase_array = second_word.downcase.split(' ')
+  #
+  #   for i in 1..second_phrase_array.length do
+  #    if  second_phrase_array[(i-1)] !~ /[aeiouy]/
+  #      puts "word"
+  #    else
+  #      puts second_phrase_array[(i-1)], "is not a word"
+  #     end
+  #   end
+  # end
+
   def anagram?
 
+    first_phrase_key = @first_phrase.gsub(/\s+/, "").split('').sort.join('')
+    second_phrase_key = @second_phrase.gsub(/\s+/, "").split('').sort.join('')
+    if first_phrase_key == second_phrase_key
+      if @first_phrase.reverse.split(' ').join('') == @second_phrase_array.join('')
+        "These words or phrases are anagrams of eachother and form a palindrome."
+      else
+        "These words or phrases are anagrams of eachother but don't form a palindrome."
+      end
+    # elsif
 
-    first_word_key = @first_word.downcase.split('').sort.join('')
-    second_word_key = @second_word.downcase.split('').sort.join('')
-    if first_word_key == second_word_key && @first_word.length == @second_word.length
-       true
     else
-       false
+       "This word or phrase is neither a word or palindrome."
     end
-
   end
 end
 
 
-puts "Please enter your first word"
-first_entry = gets.chomp
-puts "Please enter your second word"
-second_entry = gets.chomp
-word_entries = EvaluateWord.new(first_entry,second_entry)
-puts word_entries.anagram?()
+# loop do
+#   puts "Please enter your first word or phrase"
+#   first_entry = gets.chomp
+#   puts "Please enter your second word"
+#   second_entry = gets.chomp
+#
+#   word_entries = EvaluateWord.new(first_entry,second_entry)
+#   puts word_entries.anagram?
+#   puts "Press 'y' if you would like to play again, 'n' to quit"
+#   repeat = gets.chomp
+#   if repeat == 'n'
+#     break;
+#   end
+# end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,10 +5,21 @@ require('pry')
 describe('#Anagram_checker')  do
  it("returns true if the two words inputted are anagrams") do
    compare_words = EvaluateWord.new("tea", "eat")
-   expect(compare_words.anagram?()).to(eq(true))
+   expect(compare_words.anagram?()).to(eq("These words or phrases are anagrams of eachother but don't form a palindrome."))
  end
  it("accounts for entries of different cases") do
    compare_words = EvaluateWord.new("Tea", "eAt")
-   expect(compare_words.anagram?()).to(eq(true))
+   expect(compare_words.anagram?()).to(eq("These words or phrases are anagrams of eachother but don't form a palindrome."))
  end
+ it("checks whether two words are palindromes") do
+   compare_words = EvaluateWord.new("Tarawa", "A war at")
+   expect(compare_words.anagram?()).to(eq("These words or phrases are anagrams of eachother and form a palindrome."))
+
+ end
+ # it("checks to make sure that each word contains a vowel") do
+ # compare_words EvaluateWord.new("Tarawa", "A war at")
+ # expect(compare_words.anagram?()).to(eq("One or more of your entries contained text that are not words"))
+ # end
+
+
 end
